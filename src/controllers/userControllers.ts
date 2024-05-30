6d4cfb9d4b20a1707d872be2b6f65606fd6edb34
muhammad-jiku <muhammadjiku364@gmail.com>
2024-05-30 20:13:27 +0600

    task one controller func added

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import User from '../models/User';
 
+// create user
 export const createUser = async (req: Request, res: Response) => {
   try {
     const user = new User(req.body);
@@ -11,9 +12,22 @@ export const createUser = async (req: Request, res: Response) => {
   }
 };
 
+// get all users
 export const getUsers = async (req: Request, res: Response) => {
   try {
     const users = await User.find();
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+// task one: get users in new york
+export const getUsersInNewYork = async (req: Request, res: Response) => {
+  try {
+    const users = await User.aggregate([
+      { $match: { 'address.city': 'New York' } },
+    ]);
     res.status(200).send(users);
   } catch (err) {
     res.status(500).send(err);
