90e04918fa0cda9f023e495988b97fe54c045f2d
muhammad-jiku <muhammadjiku364@gmail.com>
2024-05-30 20:36:45 +0600

    task two route added

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,6 +3,7 @@ import {
   createUser,
   getUsers,
   getUsersInNewYork,
+  getUsersAndTheirFvrtMovies,
 } from '../controllers/userControllers';
 
 const userRouter = Router();
@@ -10,5 +11,9 @@ const userRouter = Router();
 userRouter.post('/', createUser);
 userRouter.get('/', getUsers);
 userRouter.get('/new-york', getUsersInNewYork);
+userRouter.get(
+  '/johndoe@example.com/favorite-movies',
+  getUsersAndTheirFvrtMovies
+);
 
 export default userRouter;
