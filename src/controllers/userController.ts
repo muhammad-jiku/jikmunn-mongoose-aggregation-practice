8e827928d363d448e497eda2feb86a5d9619eff4
muhammad-jiku <muhammadjiku364@gmail.com>
2024-05-30 12:51:38 +0600

    create user func added

---
+++ src/controllers/userController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import User from '../models/User';
+
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
