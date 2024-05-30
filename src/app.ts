75e5d3ed20020f33178abf18c62ae121b8524c70
muhammad-jiku <muhammadjiku364@gmail.com>
2024-05-30 12:23:50 +0600

    app file added

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import { config } from 'dotenv';
+config();
+
+export const app = express();
+
+app.use();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    limit: '25mb',
+    extended: true,
+  })
+);
+app.disable('x-powered-by'); // less hackers know about our stack
+
+// displaying welcome message
+app.get('/', (req: Request, res: Response) => {
+  res.send({
+    message: 'Welcome here!',
+  });
+});
