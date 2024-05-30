4ad82741353a063574e6436e214a4dfbff6a2707
muhammad-jiku <muhammadjiku364@gmail.com>
2024-05-30 20:51:09 +0600

    task three route added

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,6 +4,7 @@ import {
   getUsers,
   getUsersInNewYork,
   getUsersAndTheirFvrtMovies,
+  getPizzaLoverUsers,
 } from '../controllers/userControllers';
 
 const userRouter = Router();
@@ -15,5 +16,6 @@ userRouter.get(
   '/johndoe@example.com/favorite-movies',
   getUsersAndTheirFvrtMovies
 );
+userRouter.get('/favorite-food/pizza', getPizzaLoverUsers);
 
 export default userRouter;
