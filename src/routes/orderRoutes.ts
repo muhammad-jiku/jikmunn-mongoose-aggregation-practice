a351707a39b6362c7916745f36c96cb62bd9bec7
muhammad-jiku <muhammadjiku364@gmail.com>
2024-06-01 10:10:28 +0600

    task ten route added

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { createOrder, getOrders } from '../controllers/orderControllers';
+import {
+  createOrder,
+  getOrders,
+  getOrdersByCustomerDetails,
+} from '../controllers/orderControllers';
 
 const orderRouter = Router();
 
 orderRouter.post('/', createOrder);
 orderRouter.get('/', getOrders);
+orderRouter.get('/customer-details', getOrdersByCustomerDetails);
 
 export default orderRouter;
