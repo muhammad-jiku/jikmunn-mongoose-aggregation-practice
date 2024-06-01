42c0aa4f2207ac2606b2e98d54f31afbe51e7680
muhammad-jiku <muhammadjiku364@gmail.com>
2024-06-01 09:29:19 +0600

    get all orders controller func added

--- src/controllers/orderControllers.ts
+++ src/controllers/orderControllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import Order from '../models/Order';
 
+// create oreders
 export const createOrder = async (req: Request, res: Response) => {
   try {
     const order = new Order(req.body);
@@ -8,5 +9,15 @@ export const createOrder = async (req: Request, res: Response) => {
     res.status(201).send(order);
   } catch (err) {
     res.status(400).send(err);
+  }
+};
+
+// get all orders
+export const getOrders = async (req: Request, res: Response) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).send(orders);
+  } catch (err) {
+    res.status(500).send(err);
   }
 };
